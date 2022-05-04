213bb771a43a8805bd7eaf53a6f0c0b441c68ecc
Dorki-coder <63193570+Dorki-coder@users.noreply.github.com>
2022-05-04 16:17:44 +0700

    todo 2.0

--- js/script.js
+++ js/script.js
@@ -1,61 +1,133 @@
-let todo = [],
+async function getUsers() {
+  const response = await fetch("http://24api.ru/rest-user?page=2");
+  if (response.status == 200) {
+    let data = response.json();
+  }
+}
+// console.log(getUsers());
+async function getTask() {
+  const response = await fetch("http://24api.ru/rest-todo/items-by-id?id=60");
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  todo = await response.json();
+  out();
+  return todo;
+}
+console.log(getTask());
+
+async function deleteItem(id) {
+  const response = await fetch("http://24api.ru/rest-todo/" + id, {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  getTask();
+  return response;
+}
+
+async function deleteItems(ids) {
+  const response = await fetch("http://24api.ru/rest-todo/delete-items", {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ items: ids }),
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  getTask();
+  return response;
+}
+
+async function updateItem(id, data) {
+  const response = await fetch("http://24api.ru/rest-todo/" + id, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  return response;
+}
+
+let todo = getTask(),
   main_div = document.createElement("div"),
   main = document.querySelector("#list");
 const clear = document.querySelector("#deleteAll"),
   clearCompleted = document.querySelector("#clearCompleted");
 main_div.className = "list";
 
-if (localStorage.getItem("todo") != undefined) {
-  todo = JSON.parse(localStorage.getItem("todo"));
-  out();
+async function createItem(name) {
+  const data = {
+    name: name,
+    isDone: 0,
+    user_id: 60,
+  };
+  const response = await fetch("http://24api.ru/rest-todo", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  getTask();
+  return await response.json();
 }
 
 //Add task
 document.getElementById("btnAdd").addEventListener("click", function () {
   let temp = {};
   temp.todo = document.querySelector("input").value;
-  temp.check = false;
-  let i = todo.length;
-  todo[i] = temp;
   document.querySelector("input").value = "";
-  out();
-  localStorage.setItem("todo", JSON.stringify(todo));
+  createItem(temp.todo);
+  getTask();
 });
 
 //Delete all task
 clear.addEventListener("click", function () {
-  localStorage.clear();
-  todo = [];
-  out();
+  data = todo.map((obj) => obj.id);
+  deleteItems(data);
 });
 
 //Delete checked task
 clearCompleted.addEventListener("click", function () {
-  console.log(todo);
-  todo = todo.filter((elem) => {
-    return elem.check == false;
-  });
-  console.log(todo);
-  localStorage.setItem("todo", JSON.stringify(todo));
-  out();
+  data = todo.filter((obj) => obj.isDone == 1);
+  data = data.map((obj) => obj.id);
+  deleteItems(data);
 });
-
-//Delete task
-function deleteTask(index) {
-  todo.splice(index, 1);
-  out();
-}
 
 //checked unchecked
 function completeTask(index) {
   let todoItemsElems;
   todoItemsElems = document.querySelectorAll("span");
-  todo[index].check = !todo[index].check;
-
-  if (todo[index].check) {
+  if (todo[index].isDone == 0) todo[index].isDone = 1;
+  else todo[index].isDone = 0;
+  if (todo[index].isDone) {
     todoItemsElems[index].classList.add("checked");
   } else todoItemsElems[index].classList.remove("checked");
-  localStorage.setItem("todo", JSON.stringify(todo));
+  data = { isDone: todo[index].isDone };
+  updateItem(todo[index].id, data);
 }
 
 function out() {
@@ -66,14 +138,21 @@ function out() {
       "afterbegin",
       `
       <input onclick="completeTask(${index})" type="checkbox" ${
-        todo[index].check ? "checked" : ""
+        todo[index].isDone ? "checked" : ""
       } />
-      <span>${todo[index].todo}</span>
-      <button onclick="deleteTask(${index})" class="delete">❌</button>
+      <span>${todo[index].name}</span>
+      <button onclick="deleteItem(${todo[index].id})" class="delete">❌</button>
       `
     );
+
     main_div.append(grid_div);
     main.append(main_div);
     grid_div.className = "task";
+
+    let todoItemsElems;
+    todoItemsElems = document.querySelectorAll("span");
+    if (todo[index].isDone) {
+      todoItemsElems[index].classList.add("checked");
+    } else todoItemsElems[index].classList.remove("checked");
   }
 }
